7511d4b05a4fe94ad61e4984de7e04a6a78ccb6e
Masood Chaudhry <macx_92@live.com>
2020-02-27 16:19:10 -0500

    added required module

--- backups.ps1
+++ backups.ps1
@@ -1,3 +1,6 @@
+#Requires -Modules @{ModuleName='AWSPowerShell.NetCore';ModuleVersion='3.3.343.0'}
+
+
 #Get all DDB Tables in a variable to iterate through
 $tables= Get-DDBTables
 foreach ($table in $tables) 
