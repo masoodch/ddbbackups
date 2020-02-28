4218a5637bfb6d9a1df6a5854cde5ed797bcb77a
Masood Chaudhry <macx_92@live.com>
2020-02-28 11:08:05 -0500

    Added support for AWS regions

--- backups.ps1
+++ backups.ps1
@@ -1,9 +1,16 @@
-#Requires -Modules @{ModuleName='AWSPowerShell.NetCore';ModuleVersion='3.3.343.0'}
+#Get all regions
+$regions = (Get-AWSRegion | Where {$_.region -like "us*"}).region
 
+foreach ($region in $regions)
+{
+    Set-DefaultAWSRegion $region
+    Write-Host "Setting backups for all DynamoDB Tables in" $region
+    #Get all DDB Tables in a variable to iterate through
+    $tables= Get-DDBTables
 
-#Get all DDB Tables in a variable to iterate through
-$tables= Get-DDBTables
-foreach ($table in $tables) 
-    {
-        Update-DDBContinuousBackup -TableName $table -PointInTimeRecoverySpecification_PointInTimeRecoveryEnabled $true
-    }
+    foreach ($table in $tables) 
+        {
+           Update-DDBContinuousBackup -TableName $table -PointInTimeRecoverySpecification_PointInTimeRecoveryEnabled $true
+        }
+}
+
